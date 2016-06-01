3c3f6d3d720c4428dcf915024da335cf6c68b69c
ReblochonMasque <trustgold@gmail.com>
2016-06-01 15:42:08 +0800

    add sdksolver.py and sdksolver_unit_tests.py stub files

---
+++ sdksolver.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+sdksolver.py
+sudoku solver
+Created on Wed Jun  1 15:27:23 2016
+
+@author: fredericdupont
+"""
+
+from boardkeys import SQUARES, UNITS, PEERS
+
+
+class SDKSolver(object):
+    """
+    A general sudoku solver
+    """
+    pass
+
+
+if __name__ == '__main__':
+
+    pass
+
---
+++ sdksolver_unit_tests.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+sdksolver_unit_tests.py
+Unit Tests for sdksolver
+
+Created on Wed Jun  1 15:30:52 2016
+
+@author: fredericdupont
+"""
+
+import unittest
+from sdksolver import SDKSolver
+
+class Test_SDKSolver(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
