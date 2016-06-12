6d7a2d42bb414e92e9e3727050e533910c5d8136
ReblochonMasque <trustgold@gmail.com>
2016-06-12 13:27:18 +0800

    Add factory function to make a Grid from a string Add factory method to make a Grid from a sting Add tests Refactor class names

--- sdksolver.py
+++ sdksolver.py
@@ -7,23 +7,30 @@ Created on Wed Jun  1 15:27:23 2016
 @author: fredericdupont
 """
 
+# # Constants used
+# SQUARES = _board_.squares    # an ordered list of the squares
+# UNITS = _board_.units        # a dictionary with a square as key
+# PEERS = _board_.peers        # a dictionary with a square as key
+# DIGITS = _board_.digits      # '123456789'
+
 from boardkeys import SQUARES, UNITS, PEERS, DIGITS
 
 
-class SDKSolver(object):
+class Grid(object):
     """
-    A general sudoku solver
-    """
-    pass
-
-
-class SDKGrid(object):
-    """
-    represents a sudoku grid
+    represents a sudoku grid as a dictionary of {squares: values}
     """
     def __init__(self):
         self._grid = None
 
+    def from_string(self, chars):
+        """
+        :param chars: a string containing the values of a sudoku grid, in a row/col order
+        :return: a sudoku grid build from the input values and checked for validity
+                 but not for uniqueness of solution
+        """
+        self._grid = {key: value for key, value in zip(SQUARES, chars)}
+        return self
 
 def make_grid_from_string(values):
     """
@@ -31,10 +38,12 @@ def make_grid_from_string(values):
     using a DIGIT for value and '0' or '.' for empties
     ignores everything else
     """
+    _grid = Grid()
     chars = [char for char in values if char in DIGITS or char in '0.']
-    assert len(chars) == 81
-    return {key: value for key, value in zip(SQUARES, chars)}
+    assert len(chars) == 81, "the grid has an incorrect size"
+    return _grid.from_string(chars)
 
+# @TODO: test validity of grid
 
 if __name__ == '__main__':
 
--- tests/sdksolver_unit_tests.py
+++ tests/sdksolver_unit_tests.py
@@ -9,14 +9,92 @@ Created on Wed Jun  1 15:30:52 2016
 """
 
 import unittest
-from sdksolver import SDKSolver
+from sdksolver import Grid, make_grid_from_string
 
 
-class TestSDKSolver(unittest.TestCase):
+class TestGrid(unittest.TestCase):
 
     def setUp(self):
+        # self.grid0 = Grid()
         pass
 
+    # @TODO: test if grid is valid
+
+    def test_grid_instance(self):
+        grid0 = Grid()
+        self.assertIsInstance(grid0, Grid)
+
+    def test_grid_instance_from_chars_linear(self):
+        chars0 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+        grid0_from_chars = Grid().from_string(chars0)
+        self.assertIsInstance(grid0_from_chars, Grid)
+
+    def test_grid_instance_from_chars_block(self):
+        chars1 = """
+400000805
+030000000
+000700000
+020000060
+000080400
+000010000
+000603070
+500200000
+104000000"""
+        grid1_from_chars = Grid().from_string(chars1)
+        self.assertIsInstance(grid1_from_chars, Grid)
+
+    def test_grid_instance_from_chars_formatted(self):
+        chars2 = """
+4 . . |. . . |8 . 5
+. 3 . |. . . |. . .
+. . . |7 . . |. . .
+------+------+------
+. 2 . |. . . |. 6 .
+. . . |. 8 . |4 . .
+. . . |. 1 . |. . .
+------+------+------
+. . . |6 . 3 |. 7 .
+5 . . |2 . . |. . .
+1 . 4 |. . . |. . .
+"""
+        grid2_from_chars = Grid().from_string(chars2)
+        self.assertIsInstance(grid2_from_chars, Grid)
+
+    def test_grid_instance_factory_from_string1(self):
+        my_string0 = '4.... /.8.5.3..## :; ...slskfj.....7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+        grid0_from_string = make_grid_from_string(my_string0)
+        self.assertIsInstance(grid0_from_string, Grid)
+
+    def test_grid_instance_factory_from_string_block(self):
+        my_string1 = """
+400000805
+030000000
+000700000
+020000060
+000080400
+000010000
+000603070
+500200000
+104000000"""
+        grid1_from_string = make_grid_from_string(my_string1)
+        self.assertIsInstance(grid1_from_string, Grid)
+
+    def test_grid_instance_factory_from_string_formatted(self):
+        my_string2 = """
+4 . . |. . . |8 . 5
+. 3 . |. . . |. . .
+. . . |7 . . |. . .
+------+------+------
+. 2 . |. . . |. 6 .
+. . . |. 8 . |4 . .
+. . . |. 1 . |. . .
+------+------+------
+. . . |6 . 3 |. 7 .
+5 . . |2 . . |. . .
+1 . 4 |. . . |. . .
+"""
+        grid2_from_string = make_grid_from_string(my_string2)
+        self.assertIsInstance(grid2_from_string, Grid)
 
 if __name__ == '__main__':
     unittest.main()
