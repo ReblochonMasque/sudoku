4bc736320a366fdccfe00081727694c69ab71987
ReblochonMasque <trustgold@gmail.com>
2016-06-18 10:16:35 +0800

    Make __str__ method & tests operational to print a grid of possible values

--- sdksolver.py
+++ sdksolver.py
@@ -102,7 +102,22 @@ class Grid(object):
         return temp == SQUARES and len(temp) == len(SQUARES)
 
     def __str__(self):
-        result = ''
+        result = '   '
+        for col, char2 in enumerate('abcdefghi'):
+            result += '     ' + char2 + '     '
+            if (col + 1) % 3 == 0:
+                result += ' '
+        result += '\n   ' + '--------------------------------- ' * 3 + '\n'
+        for row, char1 in enumerate('ABCDEFGHI'):
+            result += char1 + ' |'
+            for col, char2 in enumerate('abcdefghi'):
+                result += ' ' + self._possible_values[char1 + char2] + ' '
+                if (col + 1) % 3 == 0:
+                    result += '|'
+            result += '\n'
+            if (row + 1) % 3 == 0:
+                result += '   ' + '--------------------------------- ' * 3 + '\n'
+        # print(result)
         return result
 
 # @TODO: make a grid of values
