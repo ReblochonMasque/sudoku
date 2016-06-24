214cc4f662a4d2f81fa2b0cfd5d6d8bf11480f2e
ReblochonMasque <trustgold@gmail.com>
2016-06-24 18:06:39 +0800

    Add Stubs for PuzzleSolver and Test_PuzzleSolver

---
+++ sudoku/puzzlesolver.py
@@ -0,0 +1,34 @@
+"""
+puzzlesolver.py
+:created on: 20160624
+__author__ = 'Frederic Dupont'
+:License: GPL3
+"""
+
+
+__author__ = 'Fred Dupont'
+
+
+import sys
+
+from . import puzzleconstants as p_const
+from . import puzzle
+
+
+class PuzzleSolver(object):
+    """
+    class that solves a sudoku puzzle
+    """
+
+
+
+
+
+
+
+def main(argv):
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
---
+++ tests/puzzlesolver_unit_tests.py
@@ -0,0 +1,22 @@
+"""
+filename.py
+:created on: date
+__author__ = 'Frederic Dupont'
+:License: GPL3
+"""
+
+import sys
+
+import unittest
+
+from sudoku.puzzle import Puzzle, make_grid_from_string
+from sudoku.puzzleconstants import DIGITS, SQUARES
+
+
+class TestPuzzleSolver(unittest.TestCase):
+    pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
