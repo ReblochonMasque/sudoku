cf8aec48234533600edb5c42624903f273ada806
ReblochonMasque <trustgold@gmail.com>
2016-06-24 18:32:02 +0800

    Add __init__ in PuzzleSolver to initialize a PuzzleSolver object from a Puzzle clone & tests

--- sudoku/puzzlesolver.py
+++ sudoku/puzzlesolver.py
@@ -18,17 +18,16 @@ from . import puzzle
 class PuzzleSolver(object):
     """
     class that solves a sudoku puzzle
+    takes the clone of a puzzle and solves it
     """
+    def __init__(self, puzzle_clone):
+        self._puzzle = puzzle_clone
 
 
 
-
-
-
-
-def main(argv):
-    pass
-
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+# def main(argv):
+#     pass
+#
+#
+# if __name__ == '__main__':
+#     sys.exit(main(sys.argv))
--- tests/puzzlesolver_unit_tests.py
+++ tests/puzzlesolver_unit_tests.py
@@ -1,20 +1,29 @@
 """
-filename.py
-:created on: date
+puzzlesolver_unit_tests.py
+:created on: 20160624
 __author__ = 'Frederic Dupont'
 :License: GPL3
 """
 
-import sys
+# import sys
 
 import unittest
 
 from sudoku.puzzle import Puzzle, make_grid_from_string
 from sudoku.puzzleconstants import DIGITS, SQUARES
+from sudoku.puzzlesolver import PuzzleSolver
 
 
 class TestPuzzleSolver(unittest.TestCase):
-    pass
+
+    def test_PuzzleSolver_object(self):
+        """tests if a PuzzleSolver object is created
+        """
+        puzzle_string = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+        puzzle = make_grid_from_string(puzzle_string)
+        puzzle_clone = puzzle.clone()
+        solver = PuzzleSolver(puzzle_clone)
+        self.assertIsInstance(solver, PuzzleSolver)
 
 
 
