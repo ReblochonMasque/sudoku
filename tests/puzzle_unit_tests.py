86f93ac4dad98f145bc6feddf05bfb6d95ae61c6
ReblochonMasque <trustgold@gmail.com>
2016-06-26 18:29:00 +0800

    Remove commented out stub of filter_possible_values() method from Puzzle (a Solver method)

--- tests/puzzle_unit_tests.py
+++ tests/puzzle_unit_tests.py
@@ -229,42 +229,6 @@ class TestPuzzle(unittest.TestCase):
         self.assertTrue(all(result[square] == _expected_candidates[square]
                             for square in SQUARES))
 
-    # def test_filter_remaining_possible_values_1(self):
-    #     """
-    #     tests whether the possible remaining values filtered for a given puzzle are correct
-    #     """
-    #     grid_chars_w_1_in_Aa = '1................................................................................'
-    #     valid_grid_w_1_in_Aa = make_grid_from_string(grid_chars_w_1_in_Aa)
-    #     is_valid = valid_grid_w_1_in_Aa.is_valid_grid()
-    #     self.assertTrue(is_valid)
-    #     self._exp_possible_values = {'Gh': '123456789', 'Fe': '123456789', 'Ab': '.23456789', 'Db': '123456789',
-    #                                  'Ei': '123456789', 'Ba': '.23456789', 'Ac': '.23456789', 'Eh': '123456789',
-    #                                  'Ih': '123456789', 'He': '123456789', 'Eg': '123456789', 'Hd': '123456789',
-    #                                  'Bi': '123456789', 'Cg': '123456789', 'Cc': '.23456789', 'Hi': '123456789',
-    #                                  'Af': '.23456789', 'Aa': '1........', 'Ia': '.23456789', 'Bg': '123456789',
-    #                                  'Gf': '123456789', 'Ai': '.23456789', 'Hb': '123456789', 'Ae': '.23456789',
-    #                                  'Fa': '123456789', 'Ge': '123456789', 'Hh': '123456789', 'Cd': '123456789',
-    #                                  'Ha': '.23456789', 'Fi': '123456789', 'Hf': '123456789', 'Gg': '123456789',
-    #                                  'De': '123456789', 'Df': '123456789', 'Hg': '123456789', 'Dd': '123456789',
-    #                                  'Da': '.23456789', 'Ga': '.23456789', 'Ic': '123456789', 'Bc': '.23456789',
-    #                                  'Ci': '123456789', 'Dh': '123456789', 'Bd': '123456789', 'Di': '123456789',
-    #                                  'Bf': '123456789', 'Id': '123456789', 'Cb': '.23456789', 'Gd': '123456789',
-    #                                  'Hc': '123456789', 'Gi': '123456789', 'Ch': '123456789', 'Ee': '123456789',
-    #                                  'Ea': '.23456789', 'Ff': '123456789', 'Fg': '123456789', 'Fc': '123456789',
-    #                                  'Bb': '.23456789', 'Ie': '123456789', 'If': '123456789', 'Ad': '.23456789',
-    #                                  'Eb': '123456789', 'Gb': '123456789', 'Ib': '123456789', 'Cf': '123456789',
-    #                                  'Ef': '123456789', 'Ig': '123456789', 'Dg': '123456789', 'Ah': '.23456789',
-    #                                  'Ed': '123456789', 'Fb': '123456789', 'Gc': '123456789', 'Fd': '123456789',
-    #                                  'Bh': '123456789', 'Ce': '123456789', 'Ag': '.23456789', 'Dc': '123456789',
-    #                                  'Ii': '123456789', 'Ec': '123456789', 'Ca': '.23456789', 'Fh': '123456789',
-    #                                  'Be': '123456789'}
-    #     valid_grid_w_1_in_Aa.filter_remaining_possible_values()
-    #     result = valid_grid_w_1_in_Aa.possible_values
-    #
-    #     self.assertEqual(result.keys(), self._all_possible_values.keys())
-    #     self.assertTrue(all(result[square] == self._exp_possible_values[square]
-    #                         for square in SQUARES))
-
     def test_candidates_keys_match_grid_keys(self):
         """tests that the keys of _grid and _values are the same
         """
