c66ef870e8a2361c28a11881a0b557588dbfa438
ReblochonMasque <trustgold@gmail.com>
2016-07-01 11:36:43 +0800

    Stub puzzlesolver_compound_tests file

---
+++ tests/puzzlesolver_compound_tests.py
@@ -0,0 +1,19 @@
+"""
+puzzlesolver_compound_tests.py
+:created on: 20160701
+__author__ = 'Frederic Dupont'
+:License: GPL3
+"""
+
+import unittest
+
+from sudoku.puzzle import Puzzle, make_grid_from_string
+# from sudoku.puzzleconstants import DIGITS, SQUARES
+from sudoku.puzzlesolver import PuzzleSolver
+
+
+class TestPuzzleSolver(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
