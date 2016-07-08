db7e44f9b21942a0e715156f8f3a3fda1e50e7c1
ReblochonMasque <trustgold@gmail.com>
2016-07-08 10:17:10 +0800

    Add minimal solve() - 2 tests BROKEN (they need search)

--- sudoku/puzzlesolver.py
+++ sudoku/puzzlesolver.py
@@ -14,6 +14,11 @@ import sys
 from sudoku import puzzleconstants as p_const
 from sudoku.puzzle import Puzzle, make_grid_from_string
 
+class PuzzleInvalidException(Exception):
+    """Exception raised when puzzle is invalid
+    """
+    pass
+
 
 class PuzzleSolver(object):
     """
@@ -75,13 +80,19 @@ class PuzzleSolver(object):
                     if digit in self._puzzle.candidates[square]:
                         result.append(square)
                 if len(result) == 1:
-                    self._puzzle.grid[result[0]] = digit
-                    self._puzzle.candidates[result[0]] = p_const.VALUE_TO_CANDIDATES[digit]
+                    sq = result[0]
+                    try:    # assigns digit to grid[square] no matter what, 
+                        self._puzzle.candidates[sq] = p_const.VALUE_TO_CANDIDATES[digit]
+                        self._puzzle.grid[sq] = digit
+                    except KeyError:
+                        return False
+        return True
 
     def fill_singles(self):
         """
         completes a unit that is missing only one value
         by updating the grid and the candidates values
+        :return: False if an inconsistency is discovered, True otherwise
         """
         for unit in p_const.UNIT_LISTS:
             result = []
@@ -93,8 +104,13 @@ class PuzzleSolver(object):
                 else:
                     result_string_builder[int(val) - 1] = '.'
             if len(result) == 1:
-                self._puzzle.grid[result[0]] = p_const.CANDIDATES_TO_VALUE[''.join(result_string_builder)]
-                self._puzzle.candidates[result[0]] = ''.join(result_string_builder)
+                sq = result[0]
+                self._puzzle.candidates[sq] = ''.join(result_string_builder)
+                try:
+                    self._puzzle.grid[sq] = p_const.CANDIDATES_TO_VALUE[''.join(result_string_builder)]
+                except KeyError:
+                    return False
+        return True
 
     def _get_next_square(self):
         """
@@ -111,6 +127,7 @@ class PuzzleSolver(object):
                 return open_square
             elif num_candidates < lowest_num_candidates:
                 next_square = open_square
+                lowest_num_candidates = num_candidates
         return next_square
 
     def eliminate_propagate_fill(self):
@@ -125,36 +142,134 @@ class PuzzleSolver(object):
             pre_grid_state, pre_candidates_state = repr(self._puzzle), str(self._puzzle)
             if not self.eliminate_candidates() or not self._is_valid():
                 return False
-            self.propagate()
-            self.fill_singles()
+            if not self.propagate() or not self._is_valid():
+                return False
+            if not self.fill_singles() or not self._is_valid():
+                return False
+            # @TODO: 3 times self._is_valid() to refactor when time comes
             post_grid_state, post_candidates_state = repr(self._puzzle), str(self._puzzle)
             if pre_grid_state == post_grid_state and pre_candidates_state == post_candidates_state:
                 return True
         return True
 
-    def search(self):
-        """
-        creates a new clone solver
-        assigns the next candidate value to the empty square with the less candidates
-        recursively calls solve() on the new puzzle
-        """
-        if not self._is_valid():
-            return 'stuck there'
-        if self._is_solved():
-            return str(self)
+    # def search(self):
+    #     """
+    #     creates a new clone solver
+    #     assigns the next candidate value to the empty square with the less candidates
+    #     recursively calls solve() on the new puzzle
+    #     """
+    #     temp = self
+    #     liste_candidates = []
+    #     while not self._is_solved():
+    #
+    #         next_square = self._get_next_square()
+    #         candidates = [d for d in     self._puzzle.candidates[next_square] if d not in '.0']
+    #         liste_candidates.append(list(candidates))
+    #         candidate = candidates.pop()
+    #         new_solver = self._clone()
+    #         new_solver._puzzle.grid[next_square] = candidate
+    #
+    #         temp = new_solver
+    #
+    #         if not temp.eliminate_propagate_fill():
+    #             if len(candidates) > 0:  # on change de feuille
+    #                 candidate = candidates.pop()
+    #                 new_solver = new_solver._clone()
+    #                 new_solver._puzzle.grid[next_square] = candidate
+    #             else:  # on doit changer la racine
+    #                 candidates = liste_candidates.pop()
+    #                 candidate = candidates.pop()   # pas sur de ça
+    #                 new_solver = new_solver._clone()   # pas sur de ça
+    #                 new_solver._puzzle.grid[next_square] = candidate   # pas sur de ça
+    #         # si c'est bon :
+    #         else:
+    #             continue  # recommence la boucle au début.
+    #
+    #     return str(self)
+    #
+    #     # print('ouverture de search')
+    #     # if self._is_solved():  # cas d'arrêt
+    #     #     print('is solved')
+    #     #     return str(self)
+    #     # next_square = self._get_next_square()
+    #     # candidates = [d for d in self._puzzle.candidates[next_square] if d not in '.0']
+    #     # candidate = candidates.pop()
+    #     # new_solver = self._clone()
+    #     # new_solver._puzzle.grid[next_square] = candidate
+    #     # if new_solver.eliminate_propagate_fill():
+    #     #     print('self.eliminate_propagate_fill() verifié')
+    #     #
+    #     #
+    #     #     if new_solver._is_valid():
+    #     #         print('new_solver._is_valid() verifié')
+    #     #         res = new_solver.search()
+    #     #         return str(res)
+    #     #         print('is_valid new solver')
+    #     #     else:
+    #     #         self._puzzle.grid[next_square] = candidates.pop()
+    #     #         self.search()
+    #     #         print('refus de l\'hypothèse')
+    #     # else:
+    #     #     candidate = candidates.pop()
+    #     #     # self.search()
 
-        new_solver = self._clone()
-
-        next_square = new_solver._get_next_square()
-        if next_square is None:
-            # print(new_solver)
-            return str(new_solver)
-        candidates = [d for d in new_solver._puzzle.candidates[next_square] if d not in '.0']
-
-        for candidate in candidates:
-            print('next_square =', next_square, 'candidate =', candidate, ' - ', new_solver)
-            new_solver._puzzle.grid[next_square] = candidate
-            return new_solver.search()
+    #
+    # def search(self):
+    #     """
+    #     creates a new clone solver
+    #     assigns the next candidate value to the empty square with the less candidates
+    #     recursively calls solve() on the new puzzle
+    #     """
+    #     # if not self._is_valid():
+    #     #     return 'stuck there'
+    #     # if self._is_solved():
+    #     #     return str(self)
+    #
+    #     # new_solver = self._clone()
+    #
+    #     # if not new_solver._is_valid():
+    #     #     #raise PuzzleInvalidException
+    #     #     return 'stuck there 2' #raise  error°1
+    #     if self._is_solved():
+    #         return str(self)
+    #
+    #     next_square = self._get_next_square()
+    #     candidates = [d for d in self._puzzle.candidates[next_square] if d not in '.0']
+    #
+    #     # if next_square is None:
+    #     #     # print(new_solver)
+    #     #     return str(new_solver + 'next square was None')
+    #
+    #     while len(candidates) > 0:   # or not new_solver._is_solved()  :
+    #         new_solver = self._clone()
+    #         candidate = candidates.pop()
+    #         new_solver._puzzle.grid[next_square] = candidate
+    #         if not new_solver.eliminate_propagate_fill():
+    #             continue
+    #
+    #         # if not new_solver._is_valid():
+    #         #     #new_solver._puzzle.grid[next_square] = candidate
+    #             # new_solver.eliminate_propagate_fill()
+    #             # break
+    #         else :
+    #
+    #             new_solver.search()
+    #
+    #     return str(new_solver)
+    #     # for candidate in candidates:
+    #     #     new_solver._puzzle.grid[next_square] = candidate
+    #     #     print('next_square =', next_square, 'candidate =', candidate, ' - ', new_solver)
+    #     #     #try:
+    #     #     new_solver.search()
+    #     #     if new_solver._is_valid():
+    #     #         break
+    #     #     else:
+    #     #         self.search()
+    #     #     # except PuzzleInvalidException:   # except error°1
+    #     #     #     print('coucou et vive le qwerty')
+    #     #     #     return self   # clone d'avant
+    #     # return str(new_solver)
+    #
 
 
     def solve(self):
@@ -162,79 +277,96 @@ class PuzzleSolver(object):
         manages the operations to conduct in order to solve a puzzla
         :return: a solved solver object or an error message
         """
-        self.eliminate_propagate_fill()
-        return str(self)
         # if self._is_solved():
         #     return self
         # if self.eliminate_propagate_fill():
         #     # self.eliminate_propagate_fill()
         #     return self.search()
         # self.solve()
+        self.eliminate_propagate_fill()
+        return str(self)
 
-# def main(argv):
+
+def main(argv):
+
+    # require search
+    g1 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+    partial_s1 = '4.....8.5.3..........7......2.....6.....8.4...4..1.......6.3.7.5.32.1...1.4......'
+    s1 = '417369825632158947958724316825437169791586432346912758289643571573291684164875293'
+    solve_puzzle('g1', g1, s1)
+
+    # require search
+    g2 = '1...895..5....7819........72.4..8.7.9.71.54.8.8.7..3.531.4..78.4682....3..985...1'
+    partial_s2 = '172.895.454..2781989.5142.7254938176937165428681742395315496782468271953729853641'
+    s2 = '172389564543627819896514237254938176937165428681742395315496782468271953729853641'
+    solve_puzzle('g2', g2, s2)
+    print()
+
+    # exits with "stuck there"
+    g3 = '58261..9.3..79528117928..6....4389..9..126..8..89571..25..61.79.9..72..3....496.2'
+    s3 = '582614397346795281179283564761438925935126748428957136253861479694572813817349652'
+    solve_puzzle('g3', g3, s3)
+    print()
+
+    # Expert level
+    # Exits with "stuck there"
+    g5 = '5.9........6.......1.85...3...7....8..51...4.3...4.7..9.1......7.4.1639.........4'
+    # g5 = '5.9........6.......1.85...3...7.9..8..51...4.3...4.7..9.1......7.4.1639.........4'
+    s5 = '539674821826391475417852963142769538675138249398245716951423687784516392263987154'
+    solve_puzzle('g5', g5, s5)
+    print()
+
+    # exits with key error
+    # self._puzzle.grid[result[0]] = p_const.CANDIDATES_TO_VALUE[''.join(res_string)]
+    # KeyError: '..3.5....'
+    g4 = '2.....38..........1.3..4.575.73.281.......236....8..........1....28......6...7.4.'
+    s4 = '294756381675138492183294657547362819918475236326981574759643128432819765861527943'
+    solve_puzzle('g4', g4, s4)
+    print()
+
+def solve_puzzle(name, provided_string, expected_solved_string):
+
+    print('\n\n#----N-E-X-T---T-E-S-T---' + name + '---------------------------------------------#\n')
+    print('original string                 - ', provided_string)
+
+    provided_puzzle = make_grid_from_string(provided_string)
+    expected_puzzle = make_grid_from_string(expected_solved_string)
+    solver = PuzzleSolver(provided_puzzle.clone())
+    res = solver.solve()
+    if len(res) == 81:
+        result = make_grid_from_string(res)
+    else:
+        result = make_grid_from_string('.' * 81)
+
+    print('Solution                        - ', repr(result))
+    print('Exit search/solve with          - ', res)
+
+    print('\nstarting puzzle :')
+    print(provided_puzzle.get_puzzle_str())
+    print('expected solution :')
+    print(expected_puzzle.get_puzzle_str())
+
+    print('obtained solution :')
+    print(result.get_puzzle_str())
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+#     grid_string_1 = '1...895..5....7819........72.4..8.7.9.71.54.8.8.7..3.531.4..78.4682....3..985...1'
+#     grid_1 = make_grid_from_string(grid_string_1)
+#     grid_1.parse_grid_candidates()
 #
-#     # require search
-#     g1 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
-#     partial_s1 = '4.....8.5.3..........7......2.....6.....8.4...4..1.......6.3.7.5.32.1...1.4......'
-#     s1 = '417369825632158947958724316825437169791586432346912758289643571573291684164875293'
-#     solve_puzzle('g1', g1, s1)
 #
-#     # require search
-#     g2 = '1...895..5....7819........72.4..8.7.9.71.54.8.8.7..3.531.4..78.4682....3..985...1'
-#     partial_s2 = '172.895.454..2781989.5142.7254938176937165428681742395315496782468271953729853641'
-#     s2 = '172389564543627819896514237254938176937165428681742395315496782468271953729853641'
-#     solve_puzzle('g2', g2, s2)
-#     print()
+#     solver = PuzzleSolver(grid_1.clone())
+#     solver.eliminate_candidates()
+#     solver.propagate()
+#     print('prepuzzle = \n' + solver._puzzle.get_puzzle_str())
+#     print('pre_candidate = \n' + solver._puzzle.__str__())
 #
-#     # exits with "stuck there"
-#     g3 = '58261..9.3..79528117928..6....4389..9..126..8..89571..25..61.79.9..72..3....496.2'
-#     s3 = '582614397346795281179283564761438925935126748428957136253861479694572813817349652'
-#     solve_puzzle('g3', g3, s3)
-#     print()
-#
-#     # Expert level
-#     # Exits with "stuck there"
-#     g5 = '5.9........6.......1.85...3...7....8..51...4.3...4.7..9.1......7.4.1639.........4'
-#     # g5 = '5.9........6.......1.85...3...7.9..8..51...4.3...4.7..9.1......7.4.1639.........4'
-#     s5 = '539674821826391475417852963142769538675138249398245716951423687784516392263987154'
-#     solve_puzzle('g5', g5, s5)
-#     print()
-#
-#     # exits with key error
-#     # self._puzzle.grid[result[0]] = p_const.CANDIDATES_TO_VALUE[''.join(res_string)]
-#     # KeyError: '..3.5....'
-#     g4 = '2.....38..........1.3..4.575.73.281.......236....8..........1....28......6...7.4.'
-#     s4 = '294756381675138492183294657547362819918475236326981574759643128432819765861527943'
-#     solve_puzzle('g4', g4, s4)
-#     print()
-#
-# def solve_puzzle(name, provided_string, expected_solved_string):
-#
-#     print('\n\n#----N-E-X-T---T-E-S-T---' + name + '---------------------------------------------#\n')
-#     print('original string                 - ', provided_string)
-#
-#     provided_puzzle = make_grid_from_string(provided_string)
-#     expected_puzzle = make_grid_from_string(expected_solved_string)
-#     solver = PuzzleSolver(provided_puzzle.clone())
-#     res = solver.solve()
-#     if len(res) == 81:
-#         result = make_grid_from_string(res)
-#     else:
-#         result = make_grid_from_string('.' * 81)
-#
-#     print('Solution                        - ', repr(result))
-#     print('Exit search/solve with          - ', res)
-#
-#     print('\nstarting puzzle :')
-#     print(provided_puzzle.get_puzzle_str())
-#     print('expected solution :')
-#     print(expected_puzzle.get_puzzle_str())
-#
-#     print('obtained solution :')
-#     print(result.get_puzzle_str())
-#
-#
-#
-#
-# if __name__ == '__main__':
-#     sys.exit(main(sys.argv))
+#     solver.fill_singles()
+#     #solver.eliminate_candidates()
+#     # solver.fill_singles()
+#     print('postpuzzle = \n' + solver._puzzle.get_puzzle_str())
+#     print('post_candidate = \n' + solver._puzzle.__str__())
